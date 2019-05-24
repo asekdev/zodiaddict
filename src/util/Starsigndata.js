22feadf33e91eb2225916b7683399a593394e64f
asekdev <andy.sek94@gmail.com>
2019-05-24 11:51:53 +1000

    added starsign json data

---
+++ src/util/Starsigndata.js
@@ -0,0 +1,75 @@
+import Aquarius from "../assets/images/aquarius.png";
+import Pisces from "../assets/images/pisces.png";
+import Aries from "../assets/images/aries.png";
+import Taurus from "../assets/images/taurus.png";
+import Gemini from "../assets/images/gemini.png";
+import Cancer from "../assets/images/cancer.png";
+import Leo from "../assets/images/leo.png";
+import Virgo from "../assets/images/virgo.png";
+import Libra from "../assets/images/libra.png";
+import Scorpio from "../assets/images/scorpio.png";
+import Sagittarius from "../assets/images/sagittarius.png";
+import Capricorn from "../assets/images/capricorn.png";
+
+const signData = [
+	{
+		name: "Aquarius",
+		date: "Jan 20th - Feb 18th",
+		image: Aquarius
+	},
+	{
+		name: "Pisces",
+		date: "Feb 18th - Mar 20th",
+		image: Pisces
+	},
+	{
+		name: "Aries",
+		date: "Mar 20th - Apr 20th",
+		image: Aries
+	},
+	{
+		name: "Taurus",
+		date: "Apr 20th - May 21st",
+		image: Taurus
+	},
+	{
+		name: "Gemini",
+		date: "May 21st - Jun 21st",
+		image: Gemini
+	},
+	{
+		name: "Cancer",
+		date: "Jun 21st - Jul 23rd",
+		image: Cancer
+	},
+	{
+		name: "Leo",
+		date: "Jul 23rd - Aug 23rd",
+		image: Leo
+	},
+	{
+		name: "Virgo",
+		date: "Aug 23rd - Sep 23rd",
+		image: Virgo
+	},
+	{
+		name: "Libra",
+		date: "Sep 23rd - Oct 23rd",
+		image: Libra
+	},
+	{
+		name: "Scorpio",
+		date: "Oct 23rd - Nov 22nd",
+		image: Scorpio
+	},
+	{
+		name: "Sagittarius",
+		date: "Nov 22nd - Dec 22nd",
+		image: Sagittarius
+	},
+	{
+		name: "Capricorn",
+		date: "Dec 22nd - Jan 20th",
+		image: Capricorn
+	}
+];
